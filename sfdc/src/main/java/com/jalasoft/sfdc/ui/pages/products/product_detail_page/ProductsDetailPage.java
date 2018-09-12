2e11efd64579ff9d57244d70f5840c3425cc8f68
william1605 <will.especialista@gmail.com>
2018-09-12 09:32:54 -0400

    Classes and packages deleted in Products.

--- sfdc/src/main/java/com/jalasoft/sfdc/ui/pages/products/product_detail_page/ProductsDetailPage.java
+++
@@ -1,8 +0,0 @@
-package com.jalasoft.sfdc.ui.pages.products.product_detail_page;
-
-import com.jalasoft.sfdc.ui.BasePage;
-
-public abstract class ProductsDetailPage extends BasePage {
-
-    public abstract String verifyCreateProduct();
-}
